14ab081b3d7212ac88cac2c464d79f08744108b0
Ali <malikaali469@gmail.com>
2023-12-27 03:08:30 +0500

    save

---
+++ src/IncrDec.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const IncrDec = () => {
+    return (
+        <>
+            <div className="main-div">
+                <div className="center-div">
+                    <h1>0 </h1>
+                    <div className="btn-div">
+                        <button>INCREM</button>
+                        <button>DECREM</button>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default IncrDec;
